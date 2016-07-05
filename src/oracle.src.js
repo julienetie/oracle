68ee4792a494998bb8993adb78b649feb5b18e40
Julien <j8@protonmail.ch>
2016-07-06 00:15:45 +0100

    Added EventDispatcher polyfill

--- src/oracle.src.js
+++ src/oracle.src.js
@@ -1,110 +1,104 @@
-// var fortune = document.createElement('div');
-// fortune.id = 'fortune';
-// fortune.style.position = 'absolute';
-// fortune.style.zIndex = -100;
+/**
+ * EventDispatcher
+ * @author mrdoob / http://mrdoob.com/
+ */
+var EventDispatcher=function(){};Object.assign(EventDispatcher.prototype,{addEventListener:function(i,t){void 0===this._listeners&&(this._listeners={});var e=this._listeners;void 0===e[i]&&(e[i]=[]),-1===e[i].indexOf(t)&&e[i].push(t)},hasEventListener:function(i,t){if(void 0===this._listeners)return!1;var e=this._listeners;return void 0!==e[i]&&-1!==e[i].indexOf(t)?!0:!1},removeEventListener:function(i,t){if(void 0!==this._listeners){var e=this._listeners,s=e[i];if(void 0!==s){var n=s.indexOf(t);-1!==n&&s.splice(n,1)}}},dispatchEvent:function(i){if(void 0!==this._listeners){var t=this._listeners,e=t[i.type];if(void 0!==e){i.target=this;var s=[],n=0,r=e.length;for(n=0;r>n;n++)s[n]=e[n];for(n=0;r>n;n++)s[n].call(this,i)}}}});
 
-// document.body.appendChild(fortune)
+        var is = {};
+        var fortune;
+        var statement;
+        var fortunes = [];
 
-// var mediaQueries = document.createElement('style');
-// var head = document.getElementsByTagName('head')[0];
-// /// instance 0
-// var i = 0;
-// i++;
-// console.log(i)
-// var defualtCSS = '#fortune{width: 0;}';
-// var setter = '{ #fortune{ width: ' + i + 'px; background: red;} }';
+        is.string = function(value) {
+            return typeof value === 'string';
+        }
 
-// mediaQueries.innerHTML = '@media only screen and (max-width: 40em)' + setter;
-// head.appendChild(mediaQueries);
+        is.stringOrFunction = function(value) {
+            return typeof value === 'string' || value === 'function';
+        }
 
-// window.onresize = function() {
-//     var fortuneWidth = window.getComputedStyle(fortune, null).getPropertyValue('width');
-//     if (fortuneWidth === i + 'px') {
-//         console.log('Media query true');
-//     } else {
-//         console.log('Media query false');
-//     }
-// }
-var is = {};
-var fortune;
-var statement;
-var fortunes = [];
+        is.arrayContainingObject = function(value) {
+            return value.constructor === Array && value[0].constructor === {}.constructor;
+        }
 
-is.stringOrFunction = function(value) {
-    return typeof value === 'string' || value === 'function';
-}
+        is.fortuneTruthy = function(value, i) {
+            return window.getComputedStyle(value, null).getPropertyValue('width') === (i + 1) + 'px';
+        }
 
-is.arrayContainingObject = function(value) {
-    return value.constructor === Array && value[0].constructor === {}.constructor;
-}
+        function triggerEventOrCallback(trigger) {
+            if (is.string(trigger)) {
 
-is.fortuneTruthy = function(value, i){
-    return window.getComputedStyle(value, null).getPropertyValue('width') === (i + 1) + 'px';
-}
-
-function detectChanges(fortunes) {
-    window.onresize = function() {
-        fortunes.forEach(function(fortune, i) {
-            if (is.fortuneTruthy(fortune, i)) {
-                console.log(i, 'true');
-            } else {
-                console.log(i, 'false');
             }
-        })
-    }
-}
-
-function createMediaQueryDetector(body, head, i, mediaQuery, mediaQueries, style) {
-    // Create fortune element and append to the body
-    fortune = document.createElement('div');
-    statement = '{\n' + '#⌘' + i + '{width:' + (i + 1) + 'px;}' + '\n}';
-
-    fortune.style.position = 'absolute';
-    fortune.id = '⌘' + i;
-    fortune.style.zIndex = -1000;
-    body.appendChild(fortune);
-    style.innerHTML += '\n' + mediaQuery.media + statement;
-    fortunes.push(fortune);
-
-    if (i === mediaQueries.length - 1) {
-        detectChanges(fortunes);
-    }
-}
-
-
-function setupMediaQueriesCheck(body, head, mediaQueries, style) {
-    mediaQueries.forEach(function(mediaQuery, i, mediaQueries) {
-        createMediaQueryDetector(body, head, i, mediaQuery, mediaQueries, style);
-    });
-}
-
-
-function oracle(mediaQuery, truthy, falsy) {
-    var body = document.body;
-    var head = document.getElementsByTagName('head')[0];
-    var mediaQueries;
-
-    // Create style element and append to the head
-    var style = document.createElement('style');
-
-    head.appendChild(style);
-
-    if (typeof mediaQuery === 'string' && isStringOrFunction(truthy)) {
-        var singleOptions = {
-            media: mediaQuery,
-            truthy: truthy
         }
 
-        if (is.stringOrFunction(falsy)) {
-            singleOptions.falsy = falsy;
+
+        function detectChanges(fortunes, mediaQueries) {
+            window.onresize = function() {
+                fortunes.forEach(function(fortune, i) {
+                    if (is.fortuneTruthy(fortune, i)) {
+                        if (mediaQueries[i].hasOwnProperty('truthy')) {
+                            triggerEventOrCallback(mediaQueries[i].truthy);
+                        }
+                    } else {
+                        if (mediaQueries[i].hasOwnProperty('falsy')) {
+                            triggerEventOrCallback(mediaQueries[i].falsy);
+                        }
+                    }
+                })
+            }
         }
 
-        mediaQueries = [singleOptions];
-    }
+        function createMediaQueryDetector(body, head, i, mediaQuery, mediaQueries, style) {
+            // Create fortune element and append to the body
+            fortune = document.createElement('div');
+            statement = '{\n' + '#⌘' + i + '{width:' + (i + 1) + 'px;}' + '\n}';
 
-    if (is.arrayContainingObject(mediaQuery)) {
-        mediaQueries = mediaQuery;
-    }
+            fortune.style.position = 'absolute';
+            fortune.id = '⌘' + i;
+            fortune.style.zIndex = -1000;
+            body.appendChild(fortune);
+            style.innerHTML += '\n' + mediaQuery.media + statement;
+            fortunes.push(fortune);
 
-    setupMediaQueriesCheck(body, head, mediaQueries, style);
-}
+            if (i === mediaQueries.length - 1) {
+                detectChanges(fortunes, mediaQueries);
+            }
+        }
+
+
+        function setupMediaQueriesCheck(body, head, mediaQueries, style) {
+            mediaQueries.forEach(function(mediaQuery, i, mediaQueries) {
+                createMediaQueryDetector(body, head, i, mediaQuery, mediaQueries, style);
+            });
+        }
+
+
+        function oracle(mediaQuery, truthy, falsy) {
+            var body = document.body;
+            var head = document.getElementsByTagName('head')[0];
+            var mediaQueries;
+
+            // Create style element and append to the head
+            var style = document.createElement('style');
+
+            head.appendChild(style);
+
+            if (typeof mediaQuery === 'string' && isStringOrFunction(truthy)) {
+                var singleOptions = {
+                    media: mediaQuery,
+                    truthy: truthy
+                }
+
+                if (is.stringOrFunction(falsy)) {
+                    singleOptions.falsy = falsy;
+                }
+
+                mediaQueries = [singleOptions];
+            }
+
+            if (is.arrayContainingObject(mediaQuery)) {
+                mediaQueries = mediaQuery;
+            }
+
+            setupMediaQueriesCheck(body, head, mediaQueries, style);
+        }
