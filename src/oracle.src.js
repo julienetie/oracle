79a8f66b8131a13a661e099034df46511b314feb
Julien <j8@protonmail.ch>
2016-07-06 00:41:06 +0100

    Removed EventDispatcher, added customEvent polyfill, added resizilla

--- src/oracle.src.js
+++ src/oracle.src.js
@@ -1,8 +1,22 @@
-/**
- * EventDispatcher
- * @author mrdoob / http://mrdoob.com/
- */
-var EventDispatcher=function(){};Object.assign(EventDispatcher.prototype,{addEventListener:function(i,t){void 0===this._listeners&&(this._listeners={});var e=this._listeners;void 0===e[i]&&(e[i]=[]),-1===e[i].indexOf(t)&&e[i].push(t)},hasEventListener:function(i,t){if(void 0===this._listeners)return!1;var e=this._listeners;return void 0!==e[i]&&-1!==e[i].indexOf(t)?!0:!1},removeEventListener:function(i,t){if(void 0!==this._listeners){var e=this._listeners,s=e[i];if(void 0!==s){var n=s.indexOf(t);-1!==n&&s.splice(n,1)}}},dispatchEvent:function(i){if(void 0!==this._listeners){var t=this._listeners,e=t[i.type];if(void 0!==e){i.target=this;var s=[],n=0,r=e.length;for(n=0;r>n;n++)s[n]=e[n];for(n=0;r>n;n++)s[n].call(this,i)}}}});
+// Resizilla
+!function(n){function e(){return Date.now()||(new Date).getTime()}function t(n){function e(){function n(n,e,t){return n||e?(console.warn(t),!0):!1}function e(){return c?n(t,i,u):!1}var t=window.webkitRequestAnimationFrame,i=window.requestAnimationFrame,r=screen.width<=768?!0:!1,a=!(t&&i),o=window.performance?!1:!0,u="setTimeout is being used as a substitiue forrequestAnimationFrame due to a bug within iOS 6 builds",c=a&&r&&o;return e()}function t(n){clearTimeout(n)}function i(n){var e=Date.now(),t=Math.max(h+16,e);return setTimeout(function(){n(h=t)},t-e)}function r(){return Array.prototype.filter?(s=window["request"+f]||window[l.filter(function(n){return void 0!==window[n+d]?n:void 0})+d]||i,e()?i:s):i}function a(){function n(n,e){for(var t;e<n.length;e++)if(window[n[e]]){t=window[n[e]];break}return t}var i=[];return Array.prototype.map?(l.map(function(n){return["Cancel","CancelRequest"].map(function(e){i.push(n+e+f)})}),w=window["cancel"+f]||n(i,0)||t,e()?t:w):t}function o(){return b?i:r()}function u(){return a()}function c(){b?(window.requestAnimationFrame=i,window.cancelAnimationFrame=t):(window.requestAnimationFrame=r(),window.cancelAnimationFrame=a())}var s,w,m,l=["moz","webkit"],f="AnimationFrame",d="Request"+f,h=0,v=window.mozRequestAnimationFrame,p=window.mozCancelAnimationFrame,b=v&&!p;switch(Date.now||(Date.now=function(){return(new Date).getTime()}),n){case"request":case"":m=o();break;case"cancel":m=u();break;case"native":c();break;default:throw new Error("RequestFrame parameter is not a type.")}return m}var i=t("request"),r=t("cancel"),a=function(n,t){function r(n){return this.k=this.k?null:n,this.k+=1}function a(){this.delta=e()-o,this.callHandler=this.delta>=t?n.call():i(a)}var o=e();return i(a),r(0)};n.resizilla=function(n,e,t){function i(){var i;return function(){var o=this,u=arguments,c=function(){i=0,t||n.apply(o,u)};this.instant=t&&!i,r(i),i=a(c,e),this.instant&&n.apply(o,u)}}var o=i(arguments),u=function(n){this.addEventListener?this.addEventListener("resize",n,!0):this.attachEvent("onresize",n)};(screen.width>1023||this.mobile)&&u.call(this,o)},resizilla.enableMobileResize=function(){n.mobile=!0}}(window);
+
+
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
 
         var is = {};
         var fortune;
@@ -27,13 +41,19 @@ var EventDispatcher=function(){};Object.assign(EventDispatcher.prototype,{addEve
 
         function triggerEventOrCallback(trigger) {
             if (is.string(trigger)) {
+                console.log('custom event ' + trigger);
+                // this.dispatchEvent({ type: trigger, message: ''});
+                // document.dispatchEvent( { type: 'start', message: 'vroom vroom!' } );
 
+            }else{
+
+                trigger();
             }
         }
 
 
         function detectChanges(fortunes, mediaQueries) {
-            window.onresize = function() {
+            resizilla(function() {
                 fortunes.forEach(function(fortune, i) {
                     if (is.fortuneTruthy(fortune, i)) {
                         if (mediaQueries[i].hasOwnProperty('truthy')) {
@@ -45,7 +65,7 @@ var EventDispatcher=function(){};Object.assign(EventDispatcher.prototype,{addEve
                         }
                     }
                 })
-            }
+            }, 250,true);
         }
 
         function createMediaQueryDetector(body, head, i, mediaQuery, mediaQueries, style) {
@@ -99,6 +119,5 @@ var EventDispatcher=function(){};Object.assign(EventDispatcher.prototype,{addEve
             if (is.arrayContainingObject(mediaQuery)) {
                 mediaQueries = mediaQuery;
             }
-
             setupMediaQueriesCheck(body, head, mediaQueries, style);
         }
