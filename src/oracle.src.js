527d21fc5707a435614d19a66fa623f7a96ef47b
Julien <j8@protonmail.ch>
2016-07-06 13:55:53 +0100

    Removed dispatch event polyfill

--- src/oracle.src.js
+++ src/oracle.src.js
@@ -1,36 +1,3 @@
-// Event listener polyfill
-!window.addEventListener && (function(WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
-    WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function(type, listener) {
-        var target = this;
-
-        registry.unshift([target, type, listener, function(event) {
-            event.currentTarget = target;
-            event.preventDefault = function() {
-                event.returnValue = false
-            };
-            event.stopPropagation = function() {
-                event.cancelBubble = true
-            };
-            event.target = event.srcElement || target;
-
-            listener.call(target, event);
-        }]);
-
-        this.attachEvent("on" + type, registry[0][3]);
-    };
-
-    WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function(type, listener) {
-        for (var index = 0, register; register = registry[index]; ++index) {
-            if (register[0] == this && register[1] == type && register[2] == listener) {
-                return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
-            }
-        }
-    };
-
-    WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function(eventObject) {
-        return this.fireEvent("on" + eventObject.type, eventObject);
-    };
-})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
 
 // Resizilla
 ! function(n) {
@@ -234,6 +201,7 @@ var trig = function() {
     trig();
     resizilla(trig, 200, true);
 }
+
 
 function createMediaQueryDetector(body, head, i, mediaQuery, mediaQueries, style) {
     // Create fortune element and append to the body
